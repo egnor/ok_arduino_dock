76124d7c978a11a5b675a2eb671bf9e3c8eb611a
Dan Egnor <egnor@ofb.net>
2025-12-10 10:18:11 -0800

    tweaks

--- src/ok_micro_dock.cpp
+++ src/ok_micro_dock.cpp
@@ -37,57 +37,44 @@ static uint8_t i2c_wr_rd(uint8_t addr, std::initializer_list<int> w) {
 
 bool ok_dock_init_feather_v8() {
   // Feather boards should define SDA and SCL to match standard pins
-  OK_DETAIL("Feather dock I2C setup (SCL=%d SDA=%d)", SCL, SDA);
+  OK_DETAIL("Feather dock I2C (SCL=%d SDA=%d) starting...", SCL, SDA);
   if (!Wire.setSDA(SDA) || !Wire.setSCL(SCL)) {
     OK_ERROR("Bad I2C pins (SCL=%d SDA=%d)", SCL, SDA);
     return false;
   }
 
+  Wire.begin();  // In case not already done
+
   // Feather dock v8+ uses the PI4IOE5V6408 GPIO for buttons
   // https://www.diodes.com/datasheet/download/PI4IOE5V6408.pdf
   static constexpr int gpio_i2c = 0x43;
-  Wire.beginTransmission(gpio_i2c);
-  auto const gpio_i2c_status = Wire.endTransmission();
-  if (gpio_i2c_status != 0) {
-    OK_ERROR("I2C error %d for GPIO (0x%x)", gpio_i2c_status, gpio_i2c);
+  OK_DETAIL("Feather dock button GPIO (I2C=0x%x) starting...", gpio_i2c);
+  uint8_t const gpio_id = i2c_wr_rd(gpio_i2c, {0x01});
+  if ((gpio_id & ~0x02) != 0xA0) {
+    OK_ERROR("Button GPIO (I2C=0x%x) ID 0x%02X != 0xA0", gpio_i2c, gpio_id);
+  } else if (
+    !i2c_wr(gpio_i2c, {0x01, 0x01}) ||  // soft reset
+    !i2c_wr(gpio_i2c, {0x0D, 0x07})     // pull upward for 0, 1, 2
+  ) {
+    OK_ERROR("Button GPIO setup failed");
   } else {
-    OK_DETAIL("Starting Feather dock button GPIO (I2C 0x%x)...", gpio_i2c);
-    uint8_t const id_byte = i2c_wr_rd(gpio_i2c, {0x01});
-    if ((id_byte & ~0x02) != 0xA0) {
-      OK_ERROR("Button GPIO (I2C 0x%x) ID 0x%02X != 0xA0", gpio_i2c, id_byte);
-    } else if (
-      !i2c_wr(gpio_i2c, {0x01, 0x01}) ||  // soft reset
-      !i2c_wr(gpio_i2c, {0x03, 0x00}) ||  // all input
-      !i2c_wr(gpio_i2c, {0x07, 0xF8}) ||  // disable high-Z for 0, 1, 2
-      !i2c_wr(gpio_i2c, {0x0D, 0x03}) ||  // pull upward for 0, 1, 2
-      !i2c_wr(gpio_i2c, {0x0B, 0xF8})     // enable pull for 0, 1, 2
-    ) {
-      OK_ERROR("Button GPIO setup failed");
-    } else {
-      button_init = BUTTONS_PIx6408;
-    }
+    button_type = BUTTONS_PIx6408;
   }
 
   // Feather dock v8+ uses an SSD1306-compatible 64x32 display
   // https://www.buydisplay.com/white-0-49-inch-oled-display-64x32-iic-i2c-ssd1315-connector-fpc
-  static constexpr int screen_i2c = 0x3D;
-  Wire.begin();  // In case not already done
-  Wire.beginTransmission(screen_i2c);
-  auto const screen_i2c_status = Wire.endTransmission();
-  if (screen_i2c_status != 0) {
-    OK_ERROR("I2C error %d for screen (0x%x)", screen_i2c_status, screen_i2c);
+  static constexpr int screen_i2c = 0x3C;
+  if (!i2c_wr(screen_i2c, {})) {
+    OK_ERROR("Screen I2C (0x%x) probe error", screen_i2c);
   } else {
     // See https://github.com/olikraus/u8g2/issues/2425
-    OK_DETAIL("Starting Feather dock screen (I2C 0x%x)...", screen_i2c);
-    auto const byte_cb = u8x8_byte_arduino_hw_i2c;
-    auto const gpio_cb = u8x8_gpio_and_delay_arduino;
-    u8g2_Setup_ssd1306_64x32_1f_f(&u8g2, U8G2_R2, byte_cb, gpio_cb);
-    u8g2_SetI2CAddress(&u8g2, screen_i2c << 1);
-    u8g2_InitDisplay(&u8g2);
-    u8g2_SetPowerSave(&u8g2, 0);
-    delete ok_dock_layout;
-    ok_dock_layout = new_ok_little_layout(&u8g2);
-    ok_dock_layout->line_printf(0, "\f9Starting...");
+    OK_DETAIL("Feather dock screen (I2C=0x%x) starting...", screen_i2c);
+    ok_dock_screen = new U8G2_SSD1306_64X32_1F_F_HW_I2C(U8G2_R2);
+    ok_dock_screen->setI2CAddress(screen_i2c << 1);
+    ok_dock_screen->initDisplay();
+    ok_dock_screen->setPowerSave(0);
+    ok_dock_layout = new_ok_little_layout(ok_dock_screen->getU8g2());
+    ok_dock_layout->line_printf(0, "\f11Starting...");
   }
 
   OK_DETAIL("Feather dock setup complete");
